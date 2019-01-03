e1c8bbbdf89cb20392f48533b580786fd1d33470
daniel <ddubchenko@redclick.su>
2019-01-03 23:16:25 +0300

    update code style

--- canvas-cm.js
+++ canvas-cm.js
@@ -4,7 +4,7 @@ function $c(canvas) {
 
     const ctx = canvas.getContext('2d');
 
-    this.setSize = function(x, y) {
+    this.setSize = function (x, y) {
         if (x === undefined && y === undefined) {
             canvas.width = parseInt(getComputedStyle(canvas).width);
             canvas.height = parseInt(getComputedStyle(canvas).height);
@@ -20,16 +20,16 @@ function $c(canvas) {
         return this
     };
 
-    this.line = function(sx,sy,ex,ey){
+    this.line = function (sx, sy, ex, ey) {
         let lines = [];
-        if(Array.isArray(sx)){
+        if (Array.isArray(sx)) {
             lines = sx;
         } else {
             lines = [
-                [sx,sy,ex,ey]
+                [sx, sy, ex, ey]
             ]
         }
-        lines.forEach((i)=>{
+        lines.forEach((i) => {
             this.beginPath()
                 .moveTo(i[0], i[1])
                 .lineTo(i[2], i[3])
@@ -40,7 +40,7 @@ function $c(canvas) {
         return this;
     };
 
-    this.get = function(p) {
+    this.get = function (p) {
         switch (p.toLowerCase()) {
             case 'canvas':
             case 'c':
@@ -56,10 +56,10 @@ function $c(canvas) {
         }
     };
 
-    this.set = function(s, val) {
+    this.set = function (s, val) {
         if (typeof s === 'object' && s !== null) {
             for (let i in s) {
-                if(s.hasOwnProperty(i)){
+                if (s.hasOwnProperty(i)) {
                     ctx[i] = s[i];
                 }
             }
