98ace1d8061dce1ae891e6886e020fd166c9c6af
Daniil <daniel217032001@gmail.com>
2020-04-12 13:01:15 +0300

    update webpack

---
+++ webpack/webpack.dist.js
@@ -0,0 +1,43 @@
+const Path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js')
+  },
+  output: {
+    path: Path.join(__dirname, '../dist'),
+    filename: 'js/[name].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../dist'), to: 'dist' }
+    ]),
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+    ]
+  }
+};
